d1fe294118a234da28bd0a063bc8cb29ee146b89
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-13 14:10:07 +0530

    build

---
+++ client/build/precache-manifest.c6f446efeda1dd51f453c318d5a7de86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9535e6fe8043e720f4e8002545a24505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c5e2cc9e2bf78ddd73",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c5e2cc9e2bf78ddd73",
+    "url": "/static/js/main.122da5a0.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
