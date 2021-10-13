5112b48364e63b5fc02108425690e77d0fcc9d32
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-13 20:59:49 +0530

    working fine till now

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,3 +8,6 @@ export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+export const signIn = (formData) => {};
+export const signUp = (formData) => {};
